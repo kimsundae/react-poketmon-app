43618542b195c2b3c9f749addd71aefbfa08c2bb
sksdu_2573 <naa159@naver.com>
2024-12-28 18:23:25 +0900

    검색 기능

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,80 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios'
 import PokeCard from './components/PokeCard'
+import { useDebounce } from './hooks/useDebounce'
 
 function App() {
-  const [pokemons, setPokemons] = useState([]);
-  const url = 'https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0';
+  const [allpokemons, setAllpokemons] = useState([]);
+  const [displatedPokemons, setDisplayedPokemons] = useState([]);
+  const limitNum = 20;
+  
+  const url = `https://pokeapi.co/api/v2/pokemon/?limit=1008&offset=0`;
+  const [searchTerm, setSearchTerm] = useState("");
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
   useEffect(()=>{
     fetchPokemonData();
   },[])
 
+  useEffect(()=>{
+    handleSearchInput(debouncedSearchTerm);
+  }, [debouncedSearchTerm])
+
+  const filterDisplayedPokemonData = (allpokemonsData, displayedPokemons = []) => {
+    const limit = displayedPokemons.length + limitNum;
+    const array = allpokemonsData.filter((pokemon, index) => index + 1 <= limit)
+    return array;
+  }
+
   const fetchPokemonData = async () => {
     try{
       const response = await axios.get(url);
-      console.log(response.data.results);
-      setPokemons(response.data.results);
+      setAllpokemons(response.data.results);
+      setDisplayedPokemons(filterDisplayedPokemonData(response.data.results));
     } catch(error){
       console.error(error);
+    }
+  }
+
+  const handleSearchInput = async (searchTerm) => {
+    
+    if(searchTerm.length > 0){
+      try{
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`);
+        const pokemonData = {
+          url: `https://pokeapi.co/api/v2/pokemon/${response.data.id}`,
+          name: searchTerm
+        }
+        setPokemons( [pokemonData] );
+      }catch(error){
+        setPokemons([]);
+        console.error(error);
+      }
+    } else {
+      fetchPokemonData(true);
     }
   }
 
   return (
     <article className='pt-6'>
       <header className='flex flex-col gap-2 w-full px-4 z-50'>
-        input form
+        <div className='relative z-50'>
+          <form className='relative flex justify-center items-center w-[20.5rem] h-6 rounded-lg m-auto'>
+            <input 
+              type="text" 
+              className='text-xs w-[20.5rem] h-6 px-2 py-1 bg-[hsl(214,13%,47%)] rounded-lg text-gray-300 text-center'
+              onChange={(e)=>setSearchTerm(e.target.value)}
+              value={searchTerm}
+            />
+            <button
+              type='submit'
+              className='text-xs bg-slate-900 text-slate-300 w-[2.5rem] h-6 px-2 py-1 rounded-r-lg text-center absolute right-0 hover:bg-slate-700'
+            >
+              검색
+            </button>
+          </form>
+        </div>
       </header>
       <section className='pt-6 flex flex-col justify-content items-center overflow-auto z-0'>
         <div className='flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl '>
@@ -38,6 +87,13 @@ function App() {
           )}
         </div>
       </section>
+      <div className='text-center'>
+        <button
+          onClick={()=>{fetchPokemonData(false)}} 
+          className='bg-slate-800 px-6 py-2 my-4 text-base rounded-lg font-bold text-white'>
+          더 보기
+        </button>
+      </div>
     </article>
   )
 }
